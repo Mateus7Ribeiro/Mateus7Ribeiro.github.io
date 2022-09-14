516e38fe05ef9da177f072e1828b79d90b838018
Mateus Philips <confidente737@hotmail.com>
2022-09-14 16:06:20 -0300

    Add arquivos

---
+++ TrilhaDevPhilips/SUBIR ALTERACOES.bat
@@ -0,0 +1,16 @@
+git status
+set /p "var=-----ADICIONAR ALTERACOES PARA STAGE?----- "
+git add .
+
+git status
+
+set /p "var=Qual mensagem do commit? "
+echo %var%
+
+git commit -m %var%
+
+set /p "var=-----DESEJA SUBIR ALTERACOES?----- "
+
+git push -u origin main
+
+set /p "var=-----FIM----- "
